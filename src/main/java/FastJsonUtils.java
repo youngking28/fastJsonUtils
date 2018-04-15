50a9f2fa3f2aae9d4d1466c3beb395d49a7f3ed7
yang.jing <yangjing@dkongjian.cn>
2018-04-15 19:44:25 +0800

    添加getStringWithFatherKey 通过父子节点取值(相对准确), getValue 取出第一个得到同类型的key值

--- src/main/java/FastJsonUtils.java
+++ src/main/java/FastJsonUtils.java
@@ -60,7 +60,7 @@ public class FastJsonUtils {
                     JSONArray jarray = (JSONArray) entryNode.getValue();
                     for (Object jo : jarray) {
                         result = getString(JSON.toJSONString(jo), key);
-                        if(StringUtils.isNotBlank(result)){
+                        if (StringUtils.isNotBlank(result)) {
                             return result;
                         }
                     }
@@ -73,16 +73,103 @@ public class FastJsonUtils {
     }
 
 
-        /**
-         * <pre>
-         * 将对象转为json,下划线输出
-         * 时间格式按照 2012-01-01 12:12:12<>
-         * fastjson缺省使用CamelCase，在1.2.15版本之后，fastjson支持配置PropertyNamingStrategy
-         * </pre>
-         *
-         * @param object
-         * @return
-         */
+    /**
+     * 通过父、子节点键值对获取值
+     *
+     * @param payload
+     * @param fatherKey
+     * @param key
+     * @return
+     */
+    public static String getStringWithFatherKey(String payload, String fatherKey, String key) {
+        JSONObject payloadJson = JSON.parseObject(payload);
+        return getStringWithFatherKey(payloadJson, fatherKey, key, fatherKey);
+    }
+
+    public static String getStringWithFatherKey(JSONObject payloadJson, String fatherKey, String key, String originFatherKey) {
+        Set entrySet = payloadJson.entrySet();
+
+        String result;
+        for (Object entry : entrySet) {
+            HashMap.Entry entryNode = (HashMap.Entry) entry;
+
+            String innerKey = String.valueOf(entryNode.getKey());
+
+            if (innerKey.equals(key) && fatherKey.equals(originFatherKey)) {
+                return String.valueOf(entryNode.getValue());
+            } else {
+                if (entryNode.getValue() instanceof JSONObject) {
+                    return getStringWithFatherKey((JSONObject) entryNode.getValue(), innerKey, key, originFatherKey);
+                } else if (entryNode.getValue() instanceof JSONArray) {
+                    JSONArray jarray = (JSONArray) entryNode.getValue();
+                    for (Object jo : jarray) {
+                        result = getStringWithFatherKey((JSONObject) jo, innerKey, key, originFatherKey);
+                        if (StringUtils.isNotBlank(result)) {
+                            return result;
+                        }
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+        return "查询父、子节点关系键值对不存在。请重新输入。";
+    }
+
+
+    /**
+     * 直接获取第一个key相同, 类相同的值
+     *
+     * @param payload
+     * @param key
+     * @return
+     */
+    public static <T> T getValue(String payload, String key, Class<T> clazz) {
+        JSONObject payloadJson = JSON.parseObject(payload);
+        return getValue(payloadJson, key, clazz);
+    }
+
+
+    public static <T> T getValue(JSONObject payloadJson, String key, Class<T> clazz) {
+        Set entrySet = payloadJson.entrySet();
+
+        T result;
+        for (Object entry : entrySet) {
+            HashMap.Entry entryNode = (HashMap.Entry) entry;
+
+            String innerKey = String.valueOf(entryNode.getKey());
+
+            if (innerKey.equals(key) && clazz.isInstance(entryNode.getValue())) {
+                return (T) entryNode.getValue();
+            } else {
+                if (entryNode.getValue() instanceof JSONObject) {
+                    return getValue(payloadJson.getJSONObject(innerKey), key, clazz);
+                } else if (entryNode.getValue() instanceof JSONArray) {
+                    JSONArray jarray = (JSONArray) entryNode.getValue();
+                    for (Object jo : jarray) {
+                        result = getValue((JSONObject) jo, key, clazz);
+                        if (result != null) {
+                            return result;
+                        }
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * <pre>
+     * 将对象转为json,下划线输出
+     * 时间格式按照 2012-01-01 12:12:12<>
+     * fastjson缺省使用CamelCase，在1.2.15版本之后，fastjson支持配置PropertyNamingStrategy
+     * </pre>
+     *
+     * @param object
+     * @return
+     */
     public static String toSnakeCaseJSONString(Object object) {
         SerializeConfig config = new SerializeConfig();
 
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Yang.jing on 18/4/15.
+ */
+public class TestMain {
+}
